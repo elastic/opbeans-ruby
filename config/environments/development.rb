b292adf55c0cc547f31c1ede4da2a1e6363700ef
Mikkel Malmberg <mikkel@elastic.co>
2018-04-18 13:01:53 +0200

    Fix asset compilation

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,5 +1,5 @@
 Rails.application.configure do
-    # Verifies that versions and hashed value of the package contents in the project's package.json
+  # Verifies that versions and hashed value of the package contents in the project's package.json
   config.webpacker.check_yarn_integrity = true
 
   # Settings specified here will take precedence over those in config/application.rb.
@@ -54,5 +54,4 @@ Rails.application.configure do
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
-  config.webpacker.check_yarn_integrity = false
 end
