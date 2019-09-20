c4d57140dcee434493e3a6b7d2c80939ee5d5b71
Emily Stolfo <emstolfo@gmail.com>
2019-09-20 16:08:21 +0200

    Add Lograge options to capture transaction/span/trace id

--- config/initializers/lograge.rb
+++ config/initializers/lograge.rb
@@ -1,3 +1,11 @@
 Rails.application.configure do
   config.lograge.enabled = true
+
+  config.lograge.custom_options = lambda do |event|
+    ElasticAPM.log_ids do |transaction_id, span_id, trace_id|
+      { :'transaction.id' => transaction_id,
+        :'span.id' => span_id,
+        :'trace.id' => trace_id }
+    end
+  end
 end
