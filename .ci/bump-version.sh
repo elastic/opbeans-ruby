3954d36d7d08428d6aefba70765a54f734e6700c
Victor Martinez <VictorMartinezRubio@gmail.com>
2019-11-28 11:55:50 +0000

    use docker instead of delegating the ruby installation in the worker itself

--- .ci/bump-version.sh
+++ .ci/bump-version.sh
@@ -3,8 +3,18 @@ set -euxo pipefail
 
 AGENT_VERSION="${1?Missing the APM ruby agent version}"
 
-bundle install
-bundle update elastic-apm
+## Use docker to bump the version to ensure the environment is easy to reproduce.
+docker run --rm -t \
+  --user $UID \
+  -e LOCAL_USER_ID=$UID \
+  -e HOME=/app \
+  -w /app \
+  -v "$(pwd):/app" \
+  ruby:2.6 /bin/bash -c "set -x
+    gem update --system
+    gem install bundler
+    bundle install
+    bundle update elastic-apm"
 
 # Validate whether the agent version matches
 git diff --name-only -S"elastic-apm (${AGENT_VERSION})" | grep Gemfile.lock
