0ee46847eb3fbd5b0e1330e6dacd47632f0ed7bb
Mikkel Malmberg <mikkel@elastic.co>
2019-12-10 09:12:30 +0100

    Log to STDOUT without TaggedLogging

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -85,6 +85,7 @@ Rails.application.configure do
     logger.formatter = config.log_formatter
     config.logger    = ActiveSupport::TaggedLogging.new(logger)
   end
+  config.logger           = ActiveSupport::Logger.new(STDOUT)
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
