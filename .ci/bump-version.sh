81d59dd6721e05ddf779b07ce67b96d815ec2768
Victor Martinez <victormartinezrubio@gmail.com>
2020-01-13 15:46:41 +0000

    Add debug output when failing the git diff (#39)

--- .ci/bump-version.sh
+++ .ci/bump-version.sh
@@ -21,7 +21,12 @@ docker run --rm -t \
     bundle update elastic-apm"
 
 # Validate whether the agent version matches
-git diff --name-only -S"elastic-apm (${NEW_AGENT_VERSION})" | grep Gemfile.lock
+git diff --name-only -S"elastic-apm (${NEW_AGENT_VERSION})" | grep Gemfile.lock && found=1 || found=0
+if [ ${found} -eq 0 ] ; then
+  echo 'ERROR: Agent version was not updated. See the below diff detail output:'
+  git diff --unified=0 Gemfile.lock
+  exit 1
+fi
 
 # Commit changes
 git add Gemfile.lock
